bd796015529e262285d1b187a08a380ebf8f04b7
xtehyax <anna.smcmillan@gmail.com>
2025-02-04 16:25:03 +0000

    Lesson 6.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Private Variables
-    [SerializeField] float speed = 20.0f;
+    [SerializeField] float horsePower = 40.0f;
     [SerializeField] float turnSpeed = 45.0f;
     public float horizontalInput;
     public float forwardInput;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.localPosition;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,7 +25,8 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
 
         //Make the car move forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);    //Time.deltaTime is the time between frames so you don't go faster at higher framerates
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);    //Time.deltaTime is the time between frames so you don't go faster at higher framerates
+        playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower); //AddRelativeForce is a physics based movement
 
         //Turn the car
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); 
